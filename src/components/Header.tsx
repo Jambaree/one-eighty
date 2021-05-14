445198caa2e0d17966c0bf4ef4a48e837616f7ba
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-05-14 14:57:56 -0700

    adding border for active menu item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -86,6 +86,8 @@ const Header = (props) => {
                       key={i}
                       style={{
                         listStyleType: "none",
+                        borderBottom:
+                          headerMenu === i ? "4px solid coral" : "none",
                       }}
                     >
                       <span
