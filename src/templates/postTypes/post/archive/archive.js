711c4b11a17a3e08dd7513bb64bbca64be4cfed1
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-05-26 12:57:57 -0700

    removing classnames

--- src/templates/postTypes/post/archive/archive.js
+++ src/templates/postTypes/post/archive/archive.js
@@ -40,22 +40,15 @@ const Template = (props) => {
 
       items.push(
         <li key={i}>
-          <Link
-            to={pathname}
-            className={`first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-indigo-50 bg-${
-              i === page ? "secondary" : "white"
-            }`}
-          >
-            {i}
-          </Link>
+          <Link to={pathname}>{i}</Link>
         </li>
       )
     }
 
     return (
-      <div className="py-2">
-        <nav className="block">
-          <ul className="flex pl-0 rounded list-none flex-wrap">{items}</ul>
+      <div>
+        <nav>
+          <ul>{items}</ul>
         </nav>
       </div>
     )
@@ -63,18 +56,12 @@ const Template = (props) => {
 
   return (
     <Layout {...props} seo={seo}>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            {acf?.content?.tag && (
-              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-secondary-contrast uppercase rounded-full bg-secondary">
-                {acf.content.tag}
-              </p>
-            )}
-          </div>
+      <div>
+        <div>
+          <div>{acf?.content?.tag && <p>{acf.content.tag}</p>}</div>
           {acf?.content?.headline && (
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-              <span className="relative inline-block">
+            <h2>
+              <span>
                 {/* <svg
                   viewBox="0 0 52 24"
                   fill="currentColor"
@@ -98,16 +85,12 @@ const Template = (props) => {
                   />
                 </svg> */}
               </span>
-              <span className="relative z-1">{acf.content.headline}</span>
+              <span>{acf.content.headline}</span>
             </h2>
           )}
-          {acf?.content?.text && (
-            <p className="text-base text-gray-700 md:text-lg">
-              {acf.content.text}
-            </p>
-          )}
+          {acf?.content?.text && <p>{acf.content.text}</p>}
         </div>
-        <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
+        <div>
           {posts &&
             posts
               .slice(
@@ -115,26 +98,12 @@ const Template = (props) => {
                 (page - 1) * postsPerPage + postsPerPage
               )
               .map((o) => (
-                <div
-                  key={o.id}
-                  className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20"
-                >
-                  <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-                    {o.date}
-                  </p>
-                  <Link
-                    to={o.uri}
-                    className="block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:underline"
-                    aria-label="Read article"
-                    title={o.title}
-                  >
+                <div key={o.id}>
+                  <p>{o.date}</p>
+                  <Link to={o.uri} aria-label="Read article" title={o.title}>
                     {o.title}
                   </Link>
-                  <Link
-                    to={o.uri}
-                    aria-label=""
-                    className="inline-flex items-center font-semibold transition-colors duration-200 text-primary hover:underline"
-                  >
+                  <Link to={o.uri} aria-label="">
                     Read more
                   </Link>
                 </div>
--- src/templates/postTypes/post/default/default.js
+++ src/templates/postTypes/post/default/default.js
@@ -14,23 +14,17 @@ const Template = (props) => {
 
   return (
     <Layout {...props} seo={seo}>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-screen-sm sm:text-center sm:mx-auto">
+      <div>
+        <div>
           {categories?.nodes.map((o) => (
-            <Link
-              to={o.uri}
-              key={o.databaseId}
-              className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-secondary-contrast uppercase rounded-full bg-secondary"
-            >
+            <Link to={o.uri} key={o.databaseId}>
               {o.name}
             </Link>
           ))}
 
-          <h2 className="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-            {title}
-          </h2>
-          <p className="text-base text-gray-700 md:text-lg sm:px-4">{date}</p>
-          <hr className="w-full my-8 border-gray-300" />
+          <h2>{title}</h2>
+          <p>{date}</p>
+          <hr />
         </div>
 
         {acf?.text && <TextEditor {...acf.text} />}
