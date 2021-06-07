b7eb355da92325730162198bdc1c16ee3dfcef40
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-07 16:32:50 -0700

    desktop styled

--- src/components/heroVideo/HeroVideo.tsx
+++ src/components/heroVideo/HeroVideo.tsx
@@ -9,48 +9,71 @@ const Video = (props) => {
   const { headline, subheading, videoalignment, videolink } = props
 
   return (
-    <Edges size="md" style={{ paddingBottom: 160, paddingTop: 100 }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <Box sx={{ order: videoalignment == "first" ? 2 : 1 }}>
-          {headline && (
-            <Heading
-              variant="styles.h1"
+    <Box
+      sx={{
+        paddingBottom: 160,
+        paddingTop: 100,
+        bg: "almondLight",
+      }}
+    >
+      <Edges size="md">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <Box sx={{ order: videoalignment == "first" ? 2 : 1 }}>
+            {headline && (
+              <Heading
+                variant="styles.h1"
+                sx={{
+                  my: [12, 24, 24],
+                  fontSize: 12,
+                  pb: 25,
+                  lineHeight: "65px",
+                }}
+              >
+                {headline}
+              </Heading>
+            )}
+            {subheading && (
+              <Text
+                as="p"
+                variant="text.introduction"
+                sx={{
+                  fontSize: "18px",
+                  lineHeight: "30px",
+                  mb: 96,
+                }}
+              >
+                {subheading}
+              </Text>
+            )}
+          </Box>
+          {videolink && typeof window !== "undefined" && (
+            <Box
               sx={{
-                my: [12, 24, 24],
-                fontSize: 12,
-                pb: 25,
-                lineHeight: "65px",
+                order: videoalignment == "first" ? 1 : 2,
+                maxWidth: 890,
+                width: "80%",
+                objectFit: "contain",
+                borderRadius: "8px",
+                iframe: {
+                  height: 476,
+                  borderRadius: "8px",
+                },
               }}
             >
-              {headline}
-            </Heading>
-          )}
-          {subheading && (
-            <Text
-              sx={{
-                display: "block",
-                m: ["0 12px 20px", "0 36px 36px", "0 60px 48px"],
-              }}
-            >
-              {subheading}
-            </Text>
+              <Embed url={videolink} />
+            </Box>
           )}
         </Box>
-        {videolink && typeof window !== "undefined" && (
-          <Box sx={{ order: videoalignment == "first" ? 1 : 2, maxWidth: 890 }}>
-            <Embed url={videolink} />
-          </Box>
-        )}
-      </Box>
-    </Edges>
+      </Edges>
+    </Box>
   )
 }
 
