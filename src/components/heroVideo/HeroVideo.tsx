9631f1a702dd56fbddba2d2f9aa6c7986978f823
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-08 08:44:48 -0700

    responsive styling

--- src/components/heroVideo/HeroVideo.tsx
+++ src/components/heroVideo/HeroVideo.tsx
@@ -23,18 +23,24 @@ const Video = (props) => {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            textAlign: "center",
+            textAlign: ["unset", "center", "center"],
           }}
         >
-          <Box sx={{ order: videoalignment == "first" ? 2 : 1 }}>
+          <Box
+            sx={{
+              order: videoalignment == "first" ? 2 : 1,
+              px: [0, 45, 135],
+            }}
+          >
             {headline && (
               <Heading
                 variant="styles.h1"
                 sx={{
                   my: [12, 24, 24],
                   fontSize: 12,
+                  pr: [100, 0, 0],
                   pb: 25,
-                  lineHeight: "65px",
+                  lineHeight: ["36px", "65px", "65px"],
                 }}
               >
                 {headline}
@@ -48,6 +54,7 @@ const Video = (props) => {
                   fontSize: "18px",
                   lineHeight: "30px",
                   mb: 96,
+                  pr: [35, 0, 0],
                 }}
               >
                 {subheading}
@@ -59,11 +66,11 @@ const Video = (props) => {
               sx={{
                 order: videoalignment == "first" ? 1 : 2,
                 maxWidth: 890,
-                width: "80%",
+                width: ["100%", "100%", "80%"],
                 objectFit: "contain",
                 borderRadius: "8px",
                 iframe: {
-                  height: 476,
+                  height: ["auto", 385, 476],
                   borderRadius: "8px",
                 },
               }}
