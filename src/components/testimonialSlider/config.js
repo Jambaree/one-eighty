f22257f8a70fc7f006dd1e448e801da09c8c2ab2
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-08 13:40:15 -0700

    added more space in testimonials

--- src/components/testimonialSlider/config.js
+++ src/components/testimonialSlider/config.js
@@ -16,7 +16,8 @@ const config = {
     {
       id: "subheading",
       type: "text",
-      label: "Sub Heading",
+      label: "Subheading",
+      rows: 2,
     },
     {
       id: "slide",
@@ -27,6 +28,7 @@ const config = {
           id: "quote",
           type: "text",
           label: "Quote",
+          rows: 4,
         },
         {
           id: "name",
@@ -38,6 +40,8 @@ const config = {
           id: "position",
           type: "text",
           label: "Position",
+          instructions:
+            "Type the professional position of the person who gave this quote",
         },
       ],
     },
