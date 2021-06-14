77dc66e167610ab2f09d548fe807798e93cd5a7e
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-14 10:19:23 -0700

    fix query error

--- src/templates/postTypes/page/default/default.js
+++ src/templates/postTypes/page/default/default.js
@@ -399,6 +399,8 @@ export const CollectionQuery = graphql`
                         }
                       }
                     }
+                  }
+                }
                 ... on WpDefaultTemplate_Acf_Content_Flex_Tabs {
                   fieldGroupName
                   tabs {
