8e9e5f2ef9b3a640b6b3fcdcdecb2d04ca23ffd9
Robin Zimmer <robinzimmer1989@web.de>
2021-06-18 11:15:22 -0700

    add top banner config

---
+++ src/components/topBanner/config.ts
@@ -0,0 +1,39 @@
+const config = {
+  id: "topbanner",
+  label: "Top Banner",
+  fields: [
+    {
+      id: "headline",
+      type: "text",
+      label: "Headline",
+    },
+    {
+      id: "text",
+      type: "text",
+      label: "Text",
+      rows: 3,
+    },
+    {
+      id: "color",
+      type: "select",
+      label: "Color",
+      defaultValue: "plumDark",
+      options: [
+        {
+          name: "Plum",
+          value: "plum",
+        },
+        {
+          name: "Flax",
+          value: "flax",
+        },
+        {
+          name: "Turquoise",
+          value: "turquoise",
+        },
+      ],
+    },
+  ],
+}
+
+export default config
