1b4b7bde38552e32e5e9cfff8e30c72c7fdb9856
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-29 09:25:37 -0700

    clip paths for the inset

--- src/components/textImage/TextImage.tsx
+++ src/components/textImage/TextImage.tsx
@@ -10,71 +10,79 @@ import Button from "../Button"
 import Textarea from "../Textarea"
 
 const TextImage = (props) => {
-  const {
-    image,
-    alignment,
-    headline,
-  } = props
+  const { image, alignment, headline } = props
 
   const imageData = image?.localFile && getImage(image.localFile)
 
   return (
-    <Box
-      sx={{
-        backgroundColor: 'blue180',
-      }}
-    >
+    <Box sx={{ bg: "blue180" }}>
       <Box
+        sx={{
+          height: [800, 800, 480],
+          position: "relative",
+          display: "flex",
+          flexDirection: [
+            alignment === "left" ? "column" : "column-reverse",
+            alignment === "left" ? "column" : "column-reverse",
+            alignment === "left" ? "row" : "row-reverse",
+          ],
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
           sx={{
-            position: "relative",
-            display: "flex",
-            flexDirection: alignment === "left" ? "row" : "row-reverse",
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "space-between",
+            width: ["100%", "100%", "51%"],
+            overflow: "hidden",
+            height: 480,
+            clipPath: [
+              "unset",
+              "unset",
+              "polygon(0 100%, 100% 100%, 100% 0%, 0% 0%, 0% 75%, 12% 75%, 23% 75%, 23% 80%, 12% 80%, 12% 75%, 0% 75%)",
+            ],
           }}
         >
-          <Box
-            sx={{
-              width: ["100%", "100%", "45%"],
-              overflow: "hidden",
-              height: 480,
-            }}
-          >
-            {image
-              && imageData && (
-                  <GatsbyImage
-                    image={imageData}
-                    alt={image?.altText || ""}
-                    style={{
-                      maxWidth: "100%",
-                      objectFit: "contain",
-                    }}
-                  />
-                )}
-          </Box>
-
-          <Box
-            sx={{
-              width: ["100%", null, "55%"],
-              height: "100%",
-              mt: "auto",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              pt: 36,
-            }}
-          >
-            {headline && (
-              <Heading
-                children={Parser(headline)}
-                variant='styles.h1'
-                as='h1'
-                sx={{ width: '70%', pb: 83, color: 'white' }}
-              />
-            )}
-          </Box>
+          {image && imageData && (
+            <GatsbyImage
+              image={imageData}
+              alt={image?.altText || ""}
+              style={{
+                maxWidth: "100%",
+                objectFit: "contain",
+              }}
+            />
+          )}
         </Box>
+
+        <Box
+          sx={{
+            position: ["unset", "unset", "absolute"],
+            left: ["unset", "unset", 0],
+            width: ["100%", null, "55%"],
+            height: 480,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            pt: 36,
+            backgroundColor: "blue180",
+            clipPath: [
+              "unset",
+              "unset",
+              "polygon(0 0%, 100% 0%, 100% 75%, 91% 75%, 91% 80%, 100% 80%, 100% 100%, 0% 100%)",
+            ],
+          }}
+        >
+          {headline && (
+            <Heading
+              children={Parser(headline)}
+              variant="styles.h1"
+              as="h1"
+              sx={{ width: "70%", pb: 83, color: "white" }}
+            />
+          )}
+        </Box>
+      </Box>
     </Box>
   )
 }
