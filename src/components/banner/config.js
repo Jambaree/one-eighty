8f339cd73988983b53ff9ae2f9932cbd51c7034d
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-30 11:59:21 -0700

    remove comments in config

--- src/components/banner/config.js
+++ src/components/banner/config.js
@@ -2,96 +2,11 @@ const config = {
   id: "banner",
   label: "Banner",
   fields: [
-    // {
-    //   id: "variant",
-    //   type: "select",
-    //   label: "Banner Variant",
-    //   defaultValue: "default",
-    //   options: [
-    //     {
-    //       name: "Default",
-    //       value: "default",
-    //     },
-    //     {
-    //       name: "Graphics",
-    //       value: "graphics",
-    //     },
-    //   ],
-    // },
     {
       id: "backgroundimage",
       type: "image",
       label: "Background Image",
     },
-    // {
-    //   id: "backgroundcolor",
-    //   type: "select",
-    //   label: "Background Color",
-    //   defaultValue: "White",
-    //   instructions: "Choose a background color", // optional
-    //   options: [
-    //     {
-    //       name: "White",
-    //       value: "#FFF",
-    //     },
-    //     {
-    //       name: "Light Almond",
-    //       value: "#F5F4F1",
-    //     },
-    //   ],
-    // },
-    // {
-    //   id: "textcolor",
-    //   type: "select",
-    //   label: "Text Color",
-    //   instructions: "Choose a text color", // optional
-    //   options: [
-    //     {
-    //       name: "White",
-    //       value: "#FFF",
-    //     },
-    //     {
-    //       name: "Charcoal",
-    //       value: "#2D4052",
-    //     },
-    //   ],
-    // },
-    // {
-    //   id: "headline",
-    //   type: "text",
-    //   label: "Headline",
-    // },
-    // {
-    //   id: "headlinestyle",
-    //   type: "select",
-    //   label: "Headline Style",
-    //   defaultValue: "h2",
-    //   options: [
-    //     {
-    //       name: "H1",
-    //       value: "h1",
-    //     },
-    //     {
-    //       name: "H2",
-    //       value: "h2",
-    //     },
-    //     {
-    //       name: "H3",
-    //       value: "h3",
-    //     },
-    //   ],
-    // },
-    // {
-    //   id: "text",
-    //   type: "text",
-    //   label: "Text",
-    //   rows: 6,
-    // },
-    // {
-    //   id: "link",
-    //   type: "link",
-    //   label: "Link",
-    // },
   ],
 }
 
