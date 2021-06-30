30e115135a69d1b888c4ff5359a17fe78567f456
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-06-30 12:52:44 -0700

    refactoring to backgroundimage, image placement

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,47 +3,50 @@ import { Box } from "theme-ui"
 
 // import app components
 import Stacked from "../../icons/one-eighty-stacked.svg"
+import BackgroundImage from "../BackgroundImage"
 
 const Banner = (props) => {
   const { backgroundimage } = props
 
   return (
-    <Box>
+    <Box sx={{ position: "relative", height: 300, width: "100%" }}>
+      {backgroundimage && (
+        <BackgroundImage
+          image={backgroundimage}
+          alt={backgroundimage.altText}
+        />
+      )}
       <Box
         sx={{
-          position: "relative",
-          background:
-            backgroundimage &&
-            `url(${backgroundimage.sourceUrl}) no-repeat center`,
-          backgroundSize: "cover",
+          bg: "white",
+          height: 48,
+          width: "100%",
+          clipPath: [
+            "polygon(0 0, 60% 0, 60% calc(100% - 24px), 100% calc(100% - 24px), 100% 100%, 0 100%, 0 0)",
+            "polygon(0 0, 60% 0, 60% calc(100% - 24px), 100% calc(100% - 24px), 100% 100%, 0 100%, 0 0)",
+            "polygon(0 0, calc(100% - 480px) 0, calc(100% - 480px) calc(100% - 24px), 100% calc(100% - 24px), 100% 100%, 0 100%, 0 0)",
+          ],
+        }}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          transform: [
+            "translateY(-100px)",
+            "translateY(-70px)",
+            "translateY(-28px)",
+          ],
+          width: "unset",
+          height: "100%",
+          p: "unset",
+          svg: {
+            height: "100%",
+            maxWidth: ["50%", "50%", "100%"],
+            zIndex: 1,
+          },
         }}
       >
-        <Box
-          sx={{
-            bg: "white",
-            height: 48,
-            width: "100%",
-            clipPath: [
-              "unset",
-              "unset",
-              "polygon(0 0, calc(100% - 480px) 0, calc(100% - 480px) calc(100% - 24px), 100% calc(100% - 24px), 100% 100%, 0 100%, 0 0)",
-            ],
-          }}
-        />
-        <Box
-          sx={{
-            mt: ["-6px", "-16px", "-20px"],
-            width: "unset",
-            height: "100%",
-            p: "unset",
-            svg: {
-              height: "100%",
-              maxWidth: ["50%", "75%", "100%"],
-            },
-          }}
-        >
-          <Stacked />
-        </Box>
+        <Stacked />
       </Box>
     </Box>
   )
