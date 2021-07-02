9ee22f4de3ca44cf2cb4b64e048cf98a942a8663
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-07-02 14:13:28 -0700

    styling

--- src/components/tiles/Tiles.tsx
+++ src/components/tiles/Tiles.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Box, Grid, Heading, Text } from "theme-ui"
+import { Box, Grid, Heading, Flex } from "theme-ui"
 import Parser from "html-react-parser"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
@@ -24,8 +24,8 @@ const Tiles = (props) => {
   return (
     <Box
       sx={{
-        pt: [100, 164, 164],
-        pb: [100, 164, 164],
+        pt: [100, 100, 100],
+        pb: [100, 164, 200],
         position: "relative",
         overflow: "hidden",
       }}
@@ -71,72 +71,136 @@ const Tiles = (props) => {
               />
             )}
           </Box>
-          <Grid
-            gap={[4, 4, 6]}
-            columns={2}
-            sx={{
-              px: [0, 30],
-            }}
-          >
-            {cards &&
-              cards.map((o, i) => {
-                return (
-                  <Box key={i}>
-                    <Box
-                      sx={{
-                        position: "relative",
-                        width: "100%",
-                        img: {
-                          objectFit: "contain",
-                          height: 40,
-                        },
-                      }}
-                    >
-                      {o.image && (
-                        <GatsbyImage
-                          image={o.image.url}
-                          alt={o.image.altText}
-                        />
-                      )}
-                    </Box>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: ["flex-start", "center", "center"],
-                        textAlign: ["left", "center", "center"],
-                        maxWidth: "100%",
-                        m: ["unset", "0 auto", "0 auto"],
-                        pr: [30, "unset", "unset"],
-                      }}
-                    >
-                      {o.text && <Textarea content={o.text} />}
-                    </Box>
-                  </Box>
-                )
-              })}
-            <Box
+          {option === "1" ? (
+            <Grid
+              gap={0}
+              columns={2}
               sx={{
-                gridColumn: 2,
-                display: "flex",
-                justifyContent: "flex-end",
+                height: 708,
+                px: [0, 30],
               }}
             >
-              <Box
-                variant="links.hyperlink"
-                sx={{ mr: "12px", a: { color: "black" } }}
-              >
-                <a onClick={handleClick} name="1">
-                  Option 1
-                </a>
-              </Box>
-              <Box variant="links.hyperlink" sx={{ a: { color: "black" } }}>
-                <a onClick={handleClick} name="2">
-                  Option 2
-                </a>
-              </Box>
+              {cards &&
+                cards.map((o, i) => {
+                  return (
+                    <Box key={i} sx={{ border: "1px solid #E3E3E3", p: 24 }}>
+                      <Box
+                        sx={{
+                          position: "relative",
+                          width: "100%",
+                          img: {
+                            objectFit: "contain",
+                            height: 40,
+                          },
+                        }}
+                      >
+                        {o.image && (
+                          <GatsbyImage
+                            image={o.image.url}
+                            alt={o.image.altText}
+                          />
+                        )}
+                      </Box>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: ["flex-start", "center", "center"],
+                          textAlign: ["left", "center", "center"],
+                          maxWidth: "100%",
+                          m: ["unset", "0 auto", "0 auto"],
+                          pr: [30, "unset", "unset"],
+                        }}
+                      >
+                        {o.text && <Textarea content={o.text} />}
+                      </Box>
+                    </Box>
+                  )
+                })}
+            </Grid>
+          ) : (
+            <Flex
+              sx={{
+                height: 708,
+                flexDirection: "column",
+                px: [0, 30],
+                bg: "blue",
+              }}
+            >
+              {cards &&
+                cards.map((o, i) => {
+                  return (
+                    <Box key={i}>
+                      <Box
+                        sx={{
+                          position: "relative",
+                          width: "100%",
+                          img: {
+                            objectFit: "contain",
+                            height: 40,
+                          },
+                        }}
+                      >
+                        {o.image && (
+                          <GatsbyImage
+                            image={o.image.url}
+                            alt={o.image.altText}
+                          />
+                        )}
+                      </Box>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: ["flex-start", "center", "center"],
+                          textAlign: ["left", "center", "center"],
+                          maxWidth: "100%",
+                          m: ["unset", "0 auto", "0 auto"],
+                          pr: [30, "unset", "unset"],
+                        }}
+                      >
+                        {o.text && <Textarea content={o.text} />}
+                      </Box>
+                    </Box>
+                  )
+                })}
+            </Flex>
+          )}
+          <Box
+            sx={{
+              gridColumn: 2,
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Box
+              variant="links.hyperlink"
+              sx={{
+                mr: "12px",
+                a: {
+                  color: "black",
+                  textDecoration: option === "1" && "underline",
+                },
+              }}
+            >
+              <a onClick={handleClick} name="1">
+                Option 1
+              </a>
             </Box>
-          </Grid>
+            <Box
+              variant="links.hyperlink"
+              sx={{
+                a: {
+                  color: "black",
+                  textDecoration: option === "2" && "underline",
+                },
+              }}
+            >
+              <a onClick={handleClick} name="2">
+                Option 2
+              </a>
+            </Box>
+          </Box>
         </Box>
       </Edges>
     </Box>
