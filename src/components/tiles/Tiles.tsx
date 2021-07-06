f4153759870ce6ca5aeddf6de2f91fd0cfc2046c
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-07-06 13:00:04 -0700

    refactoring, removing extra

--- src/components/tiles/Tiles.tsx
+++ src/components/tiles/Tiles.tsx
@@ -5,20 +5,10 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 // import app components
 import Textarea from "../Textarea"
-import Button from "../Button"
 import Edges from "../Edges"
-import theme from "../../theme"
 
 const Tiles = (props) => {
-  const { headline, items } = props
-  const [option, setOption] = useState(0)
-
-  const handleClick = (e) => {
-    e.preventDefault()
-    if (e.target.name !== option) {
-      setOption(e.target.name)
-    }
-  }
+  const { headline, style, cards } = props
 
   return (
     <>
@@ -46,162 +36,151 @@ const Tiles = (props) => {
             </Box>
           </Edges>
 
-          {items &&
-            items.map((o, i) => {
-              let component
-
-              switch (o.style) {
-                case "grid":
-                  component = (
-                    <Grid
-                      gap={"1px"}
-                      columns={[1, 2, 2]}
+          {cards && style === "grid" ? (
+            <Grid
+              gap={"1px"}
+              columns={[1, 2, 2]}
+              sx={{
+                height: ["auto", 708, 708],
+                // bg: ["white", "#E3E3E3", "#E3E3E3"],
+              }}
+            >
+              {cards &&
+                cards.map((o, i) => {
+                  const image =
+                    o?.image?.localFile && getImage(o.image.localFile)
+                  return (
+                    <Box
+                      key={i}
                       sx={{
-                        height: ["auto", 708, 708],
-                        bg: ["white", "#E3E3E3", "#E3E3E3"],
+                        bg: "white",
+                        p: "48px 24px",
+                        height: ["auto", 354, 354],
+                        display: "flex",
+                        overflow: "scroll",
+                        borderBottom:
+                          i === 0
+                            ? "1px solid #E3E3E3"
+                            : i === 1
+                            ? "1px solid #E3E3E3"
+                            : ["1px solid #E3E3E3", "unset", "unset"],
+                        borderRight:
+                          i === 0
+                            ? [
+                                "unset",
+                                "1px solid #E3E3E3",
+                                "1px solid #E3E3E3",
+                              ]
+                            : i === 2
+                            ? [
+                                "unset",
+                                "1px solid #E3E3E3",
+                                "1px solid #E3E3E3",
+                              ]
+                            : "unset",
                       }}
                     >
-                      {o.cards &&
-                        o.cards.map((o, i) => {
-                          const image =
-                            o?.image?.localFile && getImage(o.image.localFile)
-
-                          return (
-                            <Box
-                              key={i}
-                              sx={{
-                                bg: "white",
-                                p: "48px 24px",
-                                height: ["auto", 354, 354],
-                                display: "flex",
-                                overflow: "scroll",
-                                borderBottom: [
-                                  "1px solid #E3E3E3",
-                                  "unset",
-                                  "unset",
-                                ],
-                              }}
-                            >
-                              <Box
-                                sx={{
-                                  width: "100%",
-                                  display: "flex",
-                                  flexDirection: "column",
-                                  flexWrap: "nowrap",
-                                  justifyContent: [
-                                    "flex-start",
-                                    i > 1 ? "flex-start" : "flex-end",
-                                    i > 1 ? "flex-start" : "flex-end",
-                                  ],
-                                  alignItems:
-                                    i === 0 || i === 2
-                                      ? ["flex-start", "flex-start", "flex-end"]
-                                      : "flex-start",
-                                }}
-                              >
-                                <Box
-                                  sx={{
-                                    position: "relative",
-                                    maxWidth: ["100%", 432, 432],
-                                    width: "100%",
-                                    textAlign: "left",
-                                    img: {
-                                      objectFit: "contain",
-                                      height: 40,
-                                      mb: "12px",
-                                    },
-                                  }}
-                                >
-                                  {o.image && (
-                                    <GatsbyImage
-                                      image={image}
-                                      alt={o.image.altText}
-                                    />
-                                  )}
-                                </Box>
-                                <Box
-                                  sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    textAlign: "left",
-                                    width: "100%",
-                                    maxWidth: ["100%", "100%", 432],
-                                  }}
-                                >
-                                  {o.text && <Textarea content={o.text} />}
-                                </Box>
-                              </Box>
-                            </Box>
-                          )
-                        })}
-                    </Grid>
-                  )
-                  break
-
-                case "list":
-                  component = (
-                    <Edges size="lg">
-                      <Flex
+                      <Box
                         sx={{
-                          mt: 115,
-                          height: "auto",
+                          width: "100%",
+                          display: "flex",
                           flexDirection: "column",
-                          px: [0, 0, "10%"],
+                          flexWrap: "nowrap",
+                          justifyContent: [
+                            "flex-start",
+                            i > 1 ? "flex-start" : "flex-end",
+                            i > 1 ? "flex-start" : "flex-end",
+                          ],
+                          alignItems:
+                            i === 0 || i === 2
+                              ? ["flex-start", "flex-start", "flex-end"]
+                              : "flex-start",
                         }}
                       >
-                        {o.cards &&
-                          o.cards.map((o, i) => {
-                            const image2 =
-                              o.image?.localFile && getImage(o.image.localFile)
-                            return (
-                              <Flex
-                                key={i}
-                                sx={{
-                                  flexDirection: "row",
-                                  mb: 70,
-                                }}
-                              >
-                                <Box
-                                  sx={{
-                                    position: "relative",
-                                    width: "30%",
-                                    img: {
-                                      objectFit: "contain",
-                                      height: 40,
-                                    },
-                                  }}
-                                >
-                                  {o.image && (
-                                    <GatsbyImage
-                                      image={image2}
-                                      alt={o.image.altText}
-                                    />
-                                  )}
-                                </Box>
-                                <Box
-                                  variant="styles.h5"
-                                  sx={{
-                                    maxWidth: "70%",
-                                    textAlign: "left",
-                                  }}
-                                >
-                                  {o.text && <Textarea content={o.text} />}
-                                </Box>
-                              </Flex>
-                            )
-                          })}
-                      </Flex>
-                    </Edges>
+                        <Box
+                          sx={{
+                            position: "relative",
+                            maxWidth: ["100%", 432, 432],
+                            width: "100%",
+                            textAlign: "left",
+                            img: {
+                              objectFit: "contain",
+                              height: 40,
+                              mb: "12px",
+                            },
+                          }}
+                        >
+                          {o.image && (
+                            <GatsbyImage image={image} alt={o.image.altText} />
+                          )}
+                        </Box>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            textAlign: "left",
+                            width: "100%",
+                            maxWidth: ["100%", "100%", 432],
+                          }}
+                        >
+                          {o.text && <Textarea content={o.text} />}
+                        </Box>
+                      </Box>
+                    </Box>
                   )
-                  break
-              }
-
-              return (
-                <Box sx={{ display: i === option ? "block" : "none" }}>
-                  {component}
-                </Box>
-              )
-            })}
+                })}
+            </Grid>
+          ) : (
+            <Edges size="lg">
+              <Flex
+                sx={{
+                  mt: 115,
+                  height: "auto",
+                  flexDirection: "column",
+                  px: [0, 0, "10%"],
+                }}
+              >
+                {cards &&
+                  cards.map((o, i) => {
+                    const image2 =
+                      o.image?.localFile && getImage(o.image.localFile)
+                    return (
+                      <Flex
+                        key={i}
+                        sx={{
+                          flexDirection: "row",
+                          mb: 70,
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            position: "relative",
+                            width: "30%",
+                            img: {
+                              objectFit: "contain",
+                              height: 40,
+                            },
+                          }}
+                        >
+                          {o.image && (
+                            <GatsbyImage image={image2} alt={o.image.altText} />
+                          )}
+                        </Box>
+                        <Box
+                          variant="styles.h5"
+                          sx={{
+                            maxWidth: "70%",
+                            textAlign: "left",
+                          }}
+                        >
+                          {o.text && <Textarea content={o.text} />}
+                        </Box>
+                      </Flex>
+                    )
+                  })}
+              </Flex>
+            </Edges>
+          )}
         </Box>
       </Box>
     </>
