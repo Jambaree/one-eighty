862ae5f2aeb071bfc73e571e0eb1c648a26f0d92
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-07-07 13:47:34 -0700

    brought back topBanner

--- src/components/topBanner/config.js
+++ src/components/topBanner/config.js
@@ -1,55 +1,55 @@
-// const config = {
-//   id: "topbanner",
-//   label: "Top Banner",
-//   fields: [
-//     {
-//       id: "headline",
-//       type: "text",
-//       label: "Headline",
-//     },
-//     {
-//       id: "text",
-//       type: "text",
-//       label: "Text",
-//       rows: 3,
-//     },
-//     {
-//       id: "color",
-//       type: "select",
-//       label: "Shape & Color",
-//       defaultValue: "plum",
-//       options: [
-//         {
-//           name: "Plum",
-//           value: "plum",
-//         },
-//         {
-//           name: "Flax",
-//           value: "flax",
-//         },
-//         {
-//           name: "Turquoise",
-//           value: "turquoise",
-//         },
-//       ],
-//     },
-//     {
-//       id: "bannerbackground",
-//       type: "select",
-//       label: "Shapes or Solid",
-//       instructions: "Choose solid background or include shapes",
-//       options: [
-//         {
-//           name: "Solid",
-//           value: true,
-//         },
-//         {
-//           name: "Shapes",
-//           value: false,
-//         },
-//       ],
-//     },
-//   ],
-// }
+const config = {
+  id: "topbanner",
+  label: "Top Banner",
+  fields: [
+    {
+      id: "headline",
+      type: "text",
+      label: "Headline",
+    },
+    {
+      id: "text",
+      type: "text",
+      label: "Text",
+      rows: 3,
+    },
+    {
+      id: "color",
+      type: "select",
+      label: "Shape & Color",
+      defaultValue: "plum",
+      options: [
+        {
+          name: "Plum",
+          value: "plum",
+        },
+        {
+          name: "Flax",
+          value: "flax",
+        },
+        {
+          name: "Turquoise",
+          value: "turquoise",
+        },
+      ],
+    },
+    {
+      id: "bannerbackground",
+      type: "select",
+      label: "Shapes or Solid",
+      instructions: "Choose solid background or include shapes",
+      options: [
+        {
+          name: "Solid",
+          value: true,
+        },
+        {
+          name: "Shapes",
+          value: false,
+        },
+      ],
+    },
+  ],
+}
 
-// export default config
+export default config
