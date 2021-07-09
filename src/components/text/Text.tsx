8c4ccb7333b15c80b8d884e729be54eaa1b03491
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-07-09 12:13:12 -0700

    Text: added fadeInUp to text content

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Box } from "theme-ui"
+import { useInView } from "react-intersection-observer"
+import { jsx, css, keyframes } from "@emotion/react"
 
 // import app components
 import Edges from "../Edges"
@@ -7,13 +9,36 @@ import Textarea from "../Textarea"
 
 const Text = (props) => {
   const { text } = props
+  const { ref, inView, entry } = useInView({})
+
+  const fadeInUp = keyframes`
+    from {
+      opacity: 0;
+      transform: translate3d(0, 100%, 0);
+    }
+  
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  `
 
   return (
-    <Edges size="cmd">
-      <Box py={[4, 5, 6]} pr={[0, "100px", "300px"]}>
-        <Textarea content={text} />
-      </Box>
-    </Edges>
+    <Box ref={ref}>
+      <Edges size="cmd">
+        <Box
+          sx={{
+            animation: inView ? `${fadeInUp} 2s ease` : "none",
+            py: [4, 5, 6],
+            pr: [0, "100px", "300px"],
+          }}
+          py={[4, 5, 6]}
+          pr={[0, "100px", "300px"]}
+        >
+          <Textarea content={text} />
+        </Box>
+      </Edges>
+    </Box>
   )
 }
 
