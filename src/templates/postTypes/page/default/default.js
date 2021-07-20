2d836687c3d1b13d46521ae8c90474872263a684
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-07-20 13:25:27 -0700

    add query

--- src/templates/postTypes/page/default/default.js
+++ src/templates/postTypes/page/default/default.js
@@ -319,6 +319,10 @@ export const CollectionQuery = graphql`
                     }
                   }
                 }
+                ... on WpDefaultTemplate_Acf_Content_Flex_Numlist {
+                  fieldGroupName
+                  text
+                }
               }
             }
             fieldGroupName
