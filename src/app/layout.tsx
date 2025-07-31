ac22f3af7ca165e4b32d0eced72e0f9c41a56611
renesauve <rdasauve@gmail.com>
2025-07-31 11:57:10 -0700

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer"
 import ThemeWrapper from "./themeWrapper"
 import { getMenuItems, getOptionsPage } from "@nextwp/core"
 import { Barlow_Semi_Condensed } from "next/font/google"
+import Script from "next/script"
 
 const barlow = Barlow_Semi_Condensed({
   variable: "--font-barlow",
@@ -24,6 +25,21 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${barlow.variable}`}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CS8E2Z8SGZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CS8E2Z8SGZ');
+          `}
+        </Script>
+      </head>
       <body>
         <ThemeWrapper>
           <div className="pt-[60px] justify-center">
